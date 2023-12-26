c56cb8ebdaab36d5b3d2c54ecf8abbc8aed176a9
Dominic-go <dominic.nguyen.gos@gmail.com>
2023-12-26 16:53:39 +0700

    feat: complete unit test 3

---
+++ unit_test_3/lib/unit_test_3.dart
@@ -0,0 +1,10 @@
+import 'dart:math';
+
+bool check(int number) {
+  print(number);
+  if (number < 2) return false;
+  for (int i = 1; i < sqrt(number).round(); i++) {
+    if (number % i == 0) return false;
+  }
+  return true;
+}
