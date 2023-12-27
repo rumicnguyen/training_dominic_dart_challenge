a5e1fec40210285eae45e26424a27301b0047301
Dominic-go <dominic.nguyen.gos@gmail.com>
2023-12-27 10:10:19 +0700

    feat: remove useless code in Game 1

--- game_1/bin/game_1.dart
+++ game_1/bin/game_1.dart
@@ -1,18 +1,13 @@
 import 'package:game_1/game_1.dart' as game_1;
 
-// [1, 2, 3]
-// 1 win 3, 2 win 1, 3 win 2
-const choices = <String>['rock', 'paper', 'scissors'];
 void main(List<String> arguments) {
   int robotCore = 0, humanCore = 0, draw = 0, round = 1;
-  // bool isNotExit = true;
   print(
       '''Welcome to Rock, Paper, Scissors. \nType 'exit' to stop the game.''');
   do {
     print('Round $round.');
     String humanChoice = game_1.humanChoice();
     if (humanChoice == 'exit') {
-      // isNotExit = false;
       break;
     }
     String robotChoice = game_1.randomChoice();
--- game_1/test/game_1_test.dart
+++
@@ -1,8 +0,0 @@
-// import 'package:game_1/game_1.dart';
-// import 'package:test/test.dart';
-
-void main() {
-  // test('calculate', () {
-  //   expect(calculate(), 42);
-  // });
-}
