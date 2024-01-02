df601dc4073d47dd738617914c10db16992f6167
Dominic-go <dominic.nguyen.gos@gmail.com>
2024-01-02 09:24:48 +0700

    feat: setup gallery app

---
+++ gallery_app/android/app/src/main/kotlin/dev/flutter/gallery_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.flutter.gallery_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ gallery_app/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:gallery_app/src/app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+
+
