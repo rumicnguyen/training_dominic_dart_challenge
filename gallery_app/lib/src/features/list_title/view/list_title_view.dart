bbebfd65090ba8193de7f9d29d5ce1ca2a0464dc
Dominic-go <dominic.nguyen.gos@gmail.com>
2024-01-05 17:54:03 +0700

    feat: add sliver appbar

--- gallery_app/lib/src/features/list_title/view/list_title_view.dart
+++ gallery_app/lib/src/features/list_title/view/list_title_view.dart
@@ -1,29 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:gallery_app/src/theme/colors.dart';
 
-const String IMG_URL = 'assets/images/card_bg.jpg';
+const String IMGURL = 'assets/images/app_bar.jpg';
 
 class ListTitleView extends StatelessWidget {
   const ListTitleView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Sample2();
-  }
-}
-
-class Sample2 extends StatelessWidget {
-  const Sample2({super.key});
-
-  @override
-  Widget build(BuildContext context) {
+    Color myColor = AppColors.red_500;
     return SafeArea(
       child: Material(
         child: CustomScrollView(
           slivers: [
-            SliverPersistentHeader(
-              delegate: MyAppBar(expandedHeight: 200),
+            SliverAppBar(
+              iconTheme: IconThemeData(
+                color: myColor,
+              ),
               pinned: true,
+              snap: false,
+              floating: true,
+              backgroundColor: AppColors.blue_500,
+              expandedHeight: 200,
+              flexibleSpace: FlexibleSpaceBar(
+                title: Text(
+                  'Title App bar',
+                  style: TextStyle(
+                      color: myColor,
+                      fontSize: 22,
+                      fontWeight: FontWeight.w500),
+                ),
+                background: Image.asset(
+                  IMGURL,
+                  width: double.maxFinite,
+                  fit: BoxFit.fill,
+                ),
+              ),
             ),
             SliverList(
               delegate: SliverChildBuilderDelegate(
@@ -37,84 +49,4 @@ class Sample2 extends StatelessWidget {
       ),
     );
   }
-}
-
-class MyAppBar extends SliverPersistentHeaderDelegate {
-  final double expandedHeight;
-
-  MyAppBar({required this.expandedHeight});
-
-  @override
-  Widget build(
-      BuildContext context, double shrinkOffset, bool overlapsContent) {
-    return Stack(
-      fit: StackFit.expand,
-      children: [
-        Image.asset(
-          IMG_URL,
-          fit: BoxFit.cover,
-        ),
-        Center(
-          child: Opacity(
-            opacity: shrinkOffset / expandedHeight,
-            child: SizedBox(
-              height: 200,
-              child: Column(
-                children: [
-                  const Text(
-                    "Super Lucky Mr.Fortune Teller",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w700,
-                      fontSize: 23,
-                    ),
-                  ),
-                  Row(
-                    children: [
-                      Row(
-                        children: [
-                          Container(
-                            color: AppColors.slate_300,
-                            width: 100,
-                            height: 50,
-                            child: const Center(
-                              child: Text('data'),
-                            ),
-                          )
-                        ],
-                      )
-                    ],
-                  )
-                ],
-              ),
-            ),
-          ),
-        ),
-        Positioned(
-          top: expandedHeight / 2 - shrinkOffset,
-          left: MediaQuery.of(context).size.width / 4,
-          child: Opacity(
-            opacity: (1 - shrinkOffset / expandedHeight),
-            child: Card(
-              elevation: 10,
-              child: SizedBox(
-                height: expandedHeight,
-                width: MediaQuery.of(context).size.width / 2,
-                child: const FlutterLogo(),
-              ),
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-
-  @override
-  double get maxExtent => expandedHeight;
-
-  @override
-  double get minExtent => kToolbarHeight;
-
-  @override
-  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) => true;
 }
