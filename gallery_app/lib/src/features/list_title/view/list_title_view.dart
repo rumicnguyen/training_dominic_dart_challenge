0c022412eac0b312c0097a49d8279d9c2dc89318
Dominic-go <dominic.nguyen.gos@gmail.com>
2024-01-08 10:23:42 +0700

    feat: add stretch and stretchModes

--- gallery_app/lib/src/features/list_title/view/list_title_view.dart
+++ gallery_app/lib/src/features/list_title/view/list_title_view.dart
@@ -3,15 +3,53 @@ import 'package:gallery_app/src/theme/colors.dart';
 
 const String IMGURL = 'assets/images/app_bar.jpg';
 
-class ListTitleView extends StatelessWidget {
+class ListTitleView extends StatefulWidget {
   const ListTitleView({super.key});
 
   @override
+  State<ListTitleView> createState() => _ListTitleViewState();
+}
+
+class _ListTitleViewState extends State<ListTitleView> {
+  ScrollController? _scrollController;
+
+  bool lastStatus = true;
+  double height = 200;
+
+  void _scrollListener() {
+    if (_isShrink != lastStatus) {
+      setState(() {
+        lastStatus = _isShrink;
+      });
+    }
+  }
+
+  bool get _isShrink {
+    return _scrollController != null &&
+        _scrollController!.hasClients &&
+        _scrollController!.offset > (height - kToolbarHeight);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController()..addListener(_scrollListener);
+  }
+
+  @override
+  void dispose() {
+    _scrollController?.removeListener(_scrollListener);
+    _scrollController?.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    Color myColor = AppColors.red_500;
+    Color myColor = AppColors.slate_50;
     return SafeArea(
       child: Material(
         child: CustomScrollView(
+          physics: const BouncingScrollPhysics(),
           slivers: [
             SliverAppBar(
               iconTheme: IconThemeData(
@@ -22,6 +60,8 @@ class ListTitleView extends StatelessWidget {
               floating: true,
               backgroundColor: AppColors.blue_500,
               expandedHeight: 200,
+              stretch: true,
+              stretchTriggerOffset: 5.0,
               flexibleSpace: FlexibleSpaceBar(
                 title: Text(
                   'Title App bar',
@@ -30,10 +70,11 @@ class ListTitleView extends StatelessWidget {
                       fontSize: 22,
                       fontWeight: FontWeight.w500),
                 ),
+                stretchModes: const [StretchMode.zoomBackground],
                 background: Image.asset(
                   IMGURL,
                   width: double.maxFinite,
-                  fit: BoxFit.fill,
+                  fit: BoxFit.cover,
                 ),
               ),
             ),
